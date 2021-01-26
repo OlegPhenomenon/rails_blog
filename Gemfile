0722250fbaad1e1a3768bcf81f3fd715cb3a218e
Oleg Hasjanov <oleg.hasjanov@eestiinternet.ee>
2021-01-26 21:48:56 +0200

    Make a basic User model

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.0'
 gem 'bootstrap-sass', '3.3.7'
 gem 'world-flags'
 gem 'rails-controller-testing'
+gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 # Use sqlite3 as the database for Active Record
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -34,4 +34,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Contacts #{@ruby_title}"
   end
 
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign Up #{@ruby_title}"
+  end
 end
